d7ea69b491dabcd9940763a1ebe5d8f3edbcbeed
Juan Pablo Quispe Molina <juancode29@gmail.com>
2023-06-25 15:25:03 -0500

    ¿qué es un closure?

---
+++ src/closure/lexical.js
@@ -0,0 +1,17 @@
+const myGlobal = 0;
+
+function myFunction(){
+    const myNumber = 1;
+    console.log(myGlobal);
+    function parent(){// funcion interna
+        const inner = 2;
+        console.log(myGlobal, myNumber);
+        function child(){//funcion interna
+            console.log(myGlobal, myNumber, inner);
+        }
+        return child; // No llames a child() aquí, solo devuelve la referencia a la función para luego ser ejecutada mas adelante,
+    }
+    return parent();// Llama a parent() para obtener la referencia a la función child
+}
+var resultado = myFunction(); 
+resultado(); // Llama a resultado() para ejecutar la función child
